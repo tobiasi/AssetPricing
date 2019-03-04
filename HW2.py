f6a392f7ceb7b3b1ff76ef96b061f92dc36e8812
Tobias Ingebrigtsen <tobias_ingebrigtsen@hotmail.com>
2019-03-04 10:29:52 +0100

    Update

--- HW2.py
+++ HW2.py
@@ -12,6 +12,7 @@ c_growth  = np.log(c['PCE'])-np.log(c['PCE'].shift(1))
 sp_500    = web.get_data_yahoo(['^GSPC'],start=start,end=end)
 sp_growth = np.log(sp_500['Close'])-np.log(sp_500['Close'].shift(1)) 
 tbill     = web.get_data_fred('TB3MS',start=start,end=end)
+tbillog   = np.log(tbill/100+1)
 
 beta  = 0.99
 stdev = np.empty((0,0))
@@ -26,4 +27,16 @@ plt.xlabel('rho', fontsize=18)
 plt.ylabel('sigma', fontsize=16)
 
 # Find the Sharpe ratio
-ER = np.mean(sp_growth)
+sp_500_m    = sp_500.asfreq('M',method='bfill')
+sp_growth_m = np.log(sp_500_m['Close'])-np.log(sp_500_m['Close'].shift(1)) 
+
+
+
+#Er       = np.mean(sp_growth)*21 # Assuming 21 trading days on average per month
+Er       = np.mean(sp_growth_m)
+sigma_r  = np.std(sp_growth_m)
+rf       = np.mean(tbillog)/12
+R        = Er-rf[0]
+SR       = R/sigma_r
+
+
--- HW4effFront.py
+++ HW4effFront.py
@@ -28,6 +28,8 @@ ret_daily    = np.log(data / data.shift(1))
 ret_mean_an  = ret_daily.mean()*252
 vcov         = ret_daily.cov()*252
 print('.'*100)
+print('\n Summary statistics \n')
+print('.'*100)
 print(' \n Annualized mean returns: \n')
 print(ret_mean_an)
 print('\n Covariance matrix of returns: \n')
@@ -47,11 +49,12 @@ for ii in range(1,simLen+1):
     temp      = weights*vcov*np.transpose(weights)
     port_std  = np.append(port_std,sum(temp.sum())**0.5)
     port_ret  = np.append(port_ret,sum(weights*ret_mean_an))
-    if (ii % 100==0):
+    if (ii % 100==0 and ii != simLen+1):
         b=('Finished with iteration ' + str(ii) + ' of ' + str(len(range(1,simLen+1))))
-        sys.stdout.write('\r'+b)
+        sys.stdout.write('\r'+b) 
+    if (ii == simLen): sys.stdout.write('\r'+'Done!') 
 
-## Identify mean-variance portfolio
+## Identify mean-variance portfolio through simulation ##
 risk_free   = web.get_data_fred('TB3MS',start=end,end=end)/100
 s_p         = (port_ret-risk_free['TB3MS'][0])/port_std
 s_p_m_ind   = np.argmax(s_p)
@@ -75,7 +78,6 @@ plt.show()
 
 
 ### What if we add more assets? (Seidel) ###
-
 def get_sp500_tickers():
     resp    = requests.get('http://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
     soup    = bs.BeautifulSoup(resp.text, 'lxml')
@@ -91,10 +93,96 @@ def get_sp500_tickers():
 sp_tickers = get_sp500_tickers() 
 
 ## Fetch 20 random tickers ##
-magic_indicies = np.random.randint(1,500,20)
+magic_indicies = np.random.randint(1,500,10)
 ext_tickers    = tickers
 for ind in magic_indicies: 
     ext_tickers = np.append(ext_tickers,sp_tickers[ind])
+
+
+
+start     = dt.datetime(2005, 1, 1)
+end       = dt.datetime(2018,1,1)
+data_new  = pd.DataFrame()
+dispTicks = list(ext_tickers)
+for ticker in ext_tickers:
+    try:
+        data_new[ticker] = web.get_data_yahoo(ticker,start=start,end=end)['Close']
+    except: continue
+
+
+ret_daily_new    = np.log(data_new / data_new.shift(1))
+ret_mean_an_new  = ret_daily_new.mean()*252
+vcov_new         = ret_daily_new.cov()*252
+print('.'*100)
+print('\n Summary statistics \n')
+print('.'*100)
+print(' \n Annualized mean returns: \n')
+print(ret_mean_an_new)
+print('\n Covariance matrix of returns: \n')
+print(vcov_new)
+print('.'*100)
+
+
+
+port_std_new = []
+port_ret_new = []
+simLen   = 10000
+print('\nProgress:')
+print('.'*100 + '\n')
+for ii in range(1,simLen+1):
+    weights   = np.random.normal(0,1,len(data_new.columns))
+    weights  /= weights.sum()
+    if np.any(weights>=2) or np.any(weights<=-2): continue
+    temp          = weights*vcov_new*np.transpose(weights)
+    port_std_new  = np.append(port_std_new,sum(temp.sum())**0.5)
+    port_ret_new  = np.append(port_ret_new,sum(weights*ret_mean_an_new))
+    if (ii % 100==0 and ii != simLen):
+        b=('Finished with iteration ' + str(ii) + ' of ' + str(len(range(1,simLen+1))))
+        sys.stdout.write('\r'+b) 
+    if (ii == simLen): sys.stdout.write('\r'+'Done!') 
+    
+## Identify mean-variance portfolio through simulation ##
+risk_free       = web.get_data_fred('TB3MS',start=end,end=end)/100
+s_p_new         = (port_ret_new-risk_free['TB3MS'][0])/port_std_new
+s_p_m_ind_new   = np.argmax(s_p_new)
+min_var_ind_new = np.argmin(port_std_new)
+
+
+
+
+
+fig, ax = plt.subplots()
+ax.scatter(port_std_new, port_ret_new, c='blue')
+ax.scatter(port_std_new[s_p_m_ind_new], port_ret_new[s_p_m_ind_new], c='red',marker='D')
+ax.scatter(port_std_new[min_var_ind_new], port_ret_new[min_var_ind_new], c='red',marker='*')
+ax.plot([0,port_std_new[s_p_m_ind_new]], [risk_free['TB3MS'][0],port_ret_new[s_p_m_ind_new]],c='r')
+ax.plot([port_std_new[s_p_m_ind_new],port_std_new[s_p_m_ind_new]*3], [port_ret_new[s_p_m_ind_new],(port_ret_new[s_p_m_ind_new]-risk_free['TB3MS'][0])*3],c='r',linestyle='--')
+ax.plot([port_std[s_p_m_ind],port_std[s_p_m_ind]*3], [port_ret[s_p_m_ind],(port_ret[s_p_m_ind]-risk_free['TB3MS'][0])*3],c='g',linestyle='--')
+plt.xlim(left=0,right=2)
+plt.title("The envelope")
+plt.xlabel(r'$\sigma$')
+plt.ylabel(r'$R_p$')
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
